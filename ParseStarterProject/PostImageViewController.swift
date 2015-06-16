aad693fc75d8d5b809b53a92cd3328a705f8ecd1
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-16 12:14:26 +0100

    Added the code to post a picture.
    The user chooses an image and enters a required caption before posting to Parse.
    Alerts show whether the posting was successful or not.

--- ParseStarterProject/PostImageViewController.swift
+++ ParseStarterProject/PostImageViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import Parse
 
-class PostImageViewController: UIViewController {
+class PostImageViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    var indicator = UIActivityIndicatorView()
 
     @IBOutlet weak var imageToPost: UIImageView!
-    @IBOutlet weak var message: UITextField!
+    @IBOutlet weak var caption: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +28,93 @@ class PostImageViewController: UIViewController {
     }
     
     @IBAction func chooseImagePressed(sender: AnyObject) {
+        var image = UIImagePickerController()
+
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = true
+        self.presentViewController(image, animated: true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+
+        imageToPost.image = image
     }
 
     @IBAction func postImagePressed(sender: AnyObject) {
+        println(imageToPost.image?.description)
+
+        if imageToPost.image == nil {
+            displayAlert("No image is selected", message: "You must select an image to post")
+            return
+        }
+
+        // A caption is required
+
+        if caption.text.isEmpty {
+            displayAlert("The caption is empty", message: "You must enter a caption for the image")
+            return
+        }
+
+        // Set up a spinny thing to show we're busy
+
+        indicator = UIActivityIndicatorView(frame: self.view.frame)
+
+        indicator.backgroundColor = UIColor(white: 1.0, alpha: 0.5)     // Grey out the background
+        indicator.center = self.view.center
+        indicator.hidesWhenStopped = true
+        indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+
+        view.addSubview(indicator)
+        indicator.startAnimating()
+
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+
+        // Create a Post record with the caption and image file.
+
+        var post = PFObject(className: "Post")
+
+        post["caption"] = caption.text
+        post["userId"]  = PFUser.currentUser()?.objectId
+
+        let imageData = UIImagePNGRepresentation(imageToPost.image)
+        let imageFile = PFFile(name: "image.png", data: imageData)
+
+        post["imageFile"] = imageFile
+
+        // Post it and start listening to the user again
+
+        post.saveInBackgroundWithBlock {
+            (success, error) -> Void in
+
+            if error == nil {
+                self.displayAlert("Image Posted", message: "Your image has been posted successfully")
+                self.imageToPost.image = UIImage(named: "placeholder.png")
+                self.caption.text = ""
+            }
+            else {
+                self.displayAlert("Could not post image", message: error!.localizedDescription)
+            }
+
+            self.indicator.stopAnimating()
+            UIApplication.sharedApplication().endIgnoringInteractionEvents()
+        }
+    }
+
+    /////////////
+    // Display an alert
+
+    func displayAlert(title: String, message: String) {
+        var alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {
+            (action) -> Void in
+
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 
     /*
